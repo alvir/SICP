241d40841cc44ecf5d116f99f2fca2cf8ec5c639
Alexander Rjazantsev <ar@anahoret.com>
2013-03-21 09:57:51 +0200

    Change absolut error on relative. Add processing for negative and near zero values.

--- 1.7.scm
+++ 1.7.scm
@@ -1,12 +1,14 @@
 #lang scheme
 (define (square x) (* x x))
-(define (improve guess x) 
-  (average guess 
+(define (improve guess x)
+  (average guess
            (/ x guess)))
 (define (average x y)
   (/ (+ x y) 2))
 (define (good-enough? guess1 guess2)
-  (< (abs (- guess1 guess2)) 0.0001))
+  (< (abs (/ (- guess1 guess2)
+             guess2))
+     0.001))
 (define (sqrt-iter guess x)
   (define (iter)
     (improve guess x))
@@ -14,7 +16,10 @@
       (iter)
       (sqrt-iter (iter) x)))
 (define (sqrt x)
-  (sqrt-iter 1.0 x))
+  (cond ((< x 0) (error "Impossible in Real numbers"))
+        ((< x 0.001) 0)
+        (else (sqrt-iter 1.0 x))))
+
 (square (sqrt 30000000000))
 (square (sqrt 1))
 (square (sqrt 4))
